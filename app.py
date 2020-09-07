7dd448d79bf743c20f091864e66f7b088455ffe8
Malik Ali Haider Awan <ali114_haider@outlook.com>
2020-09-07 10:05:57 +0500

    my name was added

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+	MyName = "Malikx-Alee"
+    html = f"<h3>Sklearn Prediction Home {MyName}</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
